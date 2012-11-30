5138ea2ce7e32da38232568b7df4c1c32b159c23
Daniel Wagner <daniel@wagner-home.com>
2012-11-30 15:12:11 -0500

    make logging in handle more possible errors, per the spec given on the forum

--- Network/DGS/Misc.hs
+++ Network/DGS/Misc.hs
@@ -1,7 +1,13 @@
+{-# LANGUAGE ViewPatterns #-}
 module Network.DGS.Misc where
 
+import Control.Applicative
+import Data.List
+import Data.List.Split
+import Data.Maybe
 import Network.DGS.Monad
-import Data.ByteString.Lazy.Char8 as L
+import Network.DGS.Errors
+import qualified Data.ByteString.Lazy.Char8 as L
 
 -- | the address of the development server, @\"dragongoserver.sourceforge.net\"@
 development :: String
@@ -10,14 +16,17 @@ production  :: String
 development = "dragongoserver.sourceforge.net"
 production  = "www.dragongoserver.net"
 
-data LoginResult
-	= WrongUsername
-	| WrongPassword
-	| LoginProblem ByteString -- ^ it's a bug in the library if one of these ever gets built
+data LoginResponse
+	= LoginProblem Error
+	| LoginNoParse String -- ^ it's a bug in the library if one of these ever gets built
 	| LoginSuccess
 	deriving (Eq, Ord, Show, Read)
 
--- TODO: see http://www.dragongoserver.net/forum/read.php?forum=10&thread=33711
+-- documentation for how login.php behaves in quick mode is available at
+-- http://www.dragongoserver.net/forum/read.php?forum=10&thread=33711
+
+-- TODO: L.unpack below means we're assuming latin-1 encoding for error
+-- reporting; is that really right?
 
 -- | Almost all commands require you to be logged in. Logging in does not count
 -- against your quota; one side effect is that this will report success even if
@@ -25,16 +34,21 @@ data LoginResult
 login :: String -- ^ server, e.g. 'development' or 'production'
       -> String -- ^ user name
       -> String -- ^ password
-      -> DGS LoginResult
-login server username password = get result loc opts where
+      -> DGS LoginResponse
+login server username password = get (result . L.unpack) loc opts where
 	loc  = uri server "login.php"
 	opts = [("quick_mode", "1"), ("userid", username), ("passwd", password)]
 
-	result bs
-		| L.pack "[#Error: wrong_userid;"   `L.isPrefixOf` bs = WrongUsername
-		| L.pack "[#Error: wrong_password;" `L.isPrefixOf` bs = WrongPassword
-		| L.pack "\nOk" == bs = LoginSuccess
-		| otherwise           = LoginProblem bs
+	result "\nOk" = LoginSuccess
+	result (parseError -> Just e) = LoginProblem e
+	result s = LoginNoParse s
+
+	parseError s = findError <$> do
+		s      <- stripPrefix "[#Error: " s
+		(e, d) <- case splitOn "; " s of
+			(e:ds) -> return (e, intercalate "; " ds)
+			_      -> Nothing
+		return (UnknownError e d)
 
 -- | Phantom type to prevent the mixing of different kinds of IDs -- game IDs,
 -- tournament IDs, move IDs, user IDs, etc.
