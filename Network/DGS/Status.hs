6cc2ad3de11d581dbe15ce554d01bd7f59ec28ea
Daniel Wagner <daniel@wagner-home.com>
2013-01-17 23:37:09 -0500

    fix up the imports in N.D.Status

--- Network/DGS/Status.hs
+++ Network/DGS/Status.hs
@@ -1,8 +1,9 @@
 module Network.DGS.Status where
 
 import Network.DGS.Status.Bulletin
-import Network.DGS.Status.Message
 import Network.DGS.Status.Game
+import Network.DGS.Status.Message
+import Network.DGS.Status.MultiplayerGame
 
 -- TODO
 type Status = ([Bulletin])
